2e5620b82b18eee527b59908bf40a19ac7352215
Petitdauphin9 <155991318+Petitdauphin9@users.noreply.github.com>
2025-12-18 14:33:23 +0000

    découpage fichiers

---
+++ Coups et victoire.py
@@ -0,0 +1,6 @@
+def jouer_coup(joueur : str) -> None:
+    pass
+def valider_coup(aae):
+    pass
+def victoire(plateau : list) -> bool:
+    pass
--- Liens Utiles.md
+++ Liens Utiles.md
@@ -1,2 +1,4 @@
 Méthodologie:
+--
+
 https://docs.google.com/document/d/1W4ZgNyyUUuqRqMh_Ysv1wQjoJJVF0GybQOGStNN66T4/edit?usp=sharing
