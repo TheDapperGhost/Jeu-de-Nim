92a3e26a34019428f3bc9667cf3c6086128d56d6
TheDapperGhost <lemercierben09@gmail.com>
2026-01-08 15:40:05 +0100

    Create plateau.py

---
+++ plateau.py
@@ -0,0 +1,12 @@
+def clear() -> None:
+    os.system("cls" if os.name in ("nt", "dos") else "clear")
+
+
+def generer_partie() -> NbrAllumettes:
+    """Génère une nouvelle partie"""
+    pass
+
+
+def afficher_plateau(NbrAllumettes: int, aae: int) -> None:
+    """Affiche le plateau mis à jour"""
+    pass
