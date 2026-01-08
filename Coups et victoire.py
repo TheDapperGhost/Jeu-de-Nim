fd14911117a957bd51d24d2ba277126ba1097cfe
TheDapperGhost <lemercierben09@gmail.com>
2026-01-08 15:05:38 +0100

    Delete Coups et victoire.py

--- Coups et victoire.py
+++
@@ -1,9 +0,0 @@
-def jouer_coup(joueur : str) -> None:
-    aae = int(input("Combien d'allumettes voulez-vous retirer ?\n"))
-    
-def valider_coup(aae):
-    if 3 >= aae > 0:
-        return True
-    return False
-def victoire(plateau :list) -> bool:
-    return len(plateau) == 1
