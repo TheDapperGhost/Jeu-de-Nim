b1ab5de4303fae0dc55f7b3680d099c13015d58f
TheDapperGhost <lemercierben09@gmail.com>
2026-01-08 15:40:29 +0100

    Create Joueur.py

---
+++ Joueur.py
@@ -0,0 +1,13 @@
+def jouer_coup() -> aae:
+    """Demande au joueur de jouer son coup"""
+    pass
+
+
+def valider_coup(aae: int) -> bool:
+    """Vérifie si le coup joué est valide"""
+    pass
+
+
+def tester_victoire(aae: int) -> bool:
+    """Teste si le coup résulte à une victoire"""
+    pass
