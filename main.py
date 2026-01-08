1ee9438e9060cefbda41ddaf3ffdf6ae75ecffb8
TheDapperGhost <lemercierben09@gmail.com>
2026-01-08 15:05:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import os
 def clear():
     os.system("cls" if os.name in ("nt", "dos") else "clear")
 
-def partie(nb_joueur : int, nb_allumettes : int, plateau : list) -> list:
-    
-    return plateau
 
-def generer_plateau(nb_allumettes : int) -> list[str]:
-    plateau : list = []
-    for i in range(nb_allumettes):
-        plateau += "|"
-    return plateau
+def generer_partie:
+    """Génère une nouvelle partie"""
 
 
-nb_allumettes : int = int(input("Combien d'allumettes voulez-vous ?\n"))
-generer_plateau(nb_allumettes)
+def afficher_plateau:
+    """Affiche le plateau mis à jour"""
+
+
+def jouer_coup:
+    """Demande au joueur de jouer son coup"""
+
+
+def valider_coup:
+    """Vérifie si le coup joué est valide et résulte en une victoire WOW"""
+
+
