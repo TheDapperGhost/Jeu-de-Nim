c71c05c0b4cd1842493668e2c73755c75d132d0e
Petitdauphin9 <155991318+Petitdauphin9@users.noreply.github.com>
2026-01-15 14:57:46 +0100

    Refactor jouer_coup and validate move logic
    Updated jouer_coup function to include input validation and modified parameters for victory testing.

--- joueur.py
+++ joueur.py
@@ -1,25 +1,20 @@
-from main import clear
-
 #aae : allumettes à enlever
-def jouer_coup(joueur, aae : int) -> int:
+def jouer_coup(joueur : bool) -> int:
     """Demande au joueur de jouer son coup"""
+    aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
     while valider_coup() is False:
         print("Entrez un nombre valide d'allumettes !")
         aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
-    clear()
-    print(f"{"|"*(NbrAllumettes-aae)}")
     return aae
     
 
 
 def valider_coup(aae: int) -> bool:
     """Vérifie si le coup joué est valide"""
-    if 1 <= aae <= 3:
-        return True
-    return False
+    return <=0 aae <=3
     
 
 
-def tester_victoire(aae: int) -> bool:
+def tester_victoire(NbrAllumettes : int, aae: int) -> bool:
     """Teste si le coup résulte à une victoire"""
     return (NbrAllumettes-aae) <= 0
