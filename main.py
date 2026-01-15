66c55ad70cb8646c2cbfc31201a4f21d8793f16f
Petitdauphin9 <155991318+Petitdauphin9@users.noreply.github.com>
2026-01-15 15:42:57 +0100

    Integrate MP3 playback on startup
    Added functionality to play an MP3 file at startup.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import plateau
 import joueur
+import mp3play
+
+url = "./canard.mp3"
+clip = mp3play.load(url)
+clip.play()
 
 def clear() -> None:
     os.system("cls" if os.name in ("nt", "dos") else "clear")
