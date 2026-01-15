55db86dec5da4b2defe2a422a4d22359173ea975
Petitdauphin9 <155991318+Petitdauphin9@users.noreply.github.com>
2026-01-15 14:44:52 +0100

    Modify jouer_coup to include player parameter

--- Joueur.py
+++ Joueur.py
@@ -1,19 +1,25 @@
 from main import clear
 
 #aae : allumettes à enlever
-def jouer_coup(aae : int) -> int:
+def jouer_coup(joueur, aae : int) -> int:
     """Demande au joueur de jouer son coup"""
+    while valider_coup() is False:
+        print("Entrez un nombre valide d'allumettes !")
+        aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
     clear()
-    
+    print(f"{"|"*(NbrAllumettes-aae)}")
     return aae
     
 
 
 def valider_coup(aae: int) -> bool:
     """Vérifie si le coup joué est valide"""
-    return valid
+    if 1 <= aae <= 3:
+        return True
+    return False
     
 
 
 def tester_victoire(aae: int) -> bool:
     """Teste si le coup résulte à une victoire"""
+    
