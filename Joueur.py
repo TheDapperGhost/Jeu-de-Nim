d5c8d2f032560b94935984ea446ae6e0ec6c8c89
Laporte-Bidet Raphaël <rlbidet@laposte.net>
2026-01-15 14:18:03 +0100

    main improvements and beginning of joueur

--- Joueur.py
+++ Joueur.py
@@ -1,15 +1,16 @@
-def jouer_coup() -> int:
+#aae : allumettes à enlever
+def jouer_coup(aae : int) -> int:
     """Demande au joueur de jouer son coup"""
     return aae
-    pass
+    
 
 
 def valider_coup(aae: int) -> bool:
     """Vérifie si le coup joué est valide"""
     return valid
-    pass
+    
 
 
 def tester_victoire(aae: int) -> bool:
     """Teste si le coup résulte à une victoire"""
-    pass
+    
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-pass
+import plateau
+import joueur
+
+plateau.generer_partie()
+
+while not joueur.tester_victoire():
+    aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
+    joueur.jouer_coup()
