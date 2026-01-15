ce8827d66ffa3a4e3dead10cd770fc7b20ed10c6
Petitdauphin9 <155991318+Petitdauphin9@users.noreply.github.com>
2026-01-15 14:58:15 +0100

    Refactor player turn logic for clarity

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ def clear() -> None:
     os.system("cls" if os.name in ("nt", "dos") else "clear")
 
 plateau.generer_partie()
-joueur = "A"
+joueur1 = True
 
 while not joueur.tester_victoire():
     aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
     joueur.jouer_coup(joueur, aae)
-    if joueur == "A":
-        joueur = "B"
-    else:
-        joueur = "A"
+    joueur1 = not joueur1
