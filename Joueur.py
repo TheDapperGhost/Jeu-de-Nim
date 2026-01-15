fc49c0c4022168189d38b29d46979a474ca013dd
Laporte-Bidet Raphaël <rlbidet@laposte.net>
2026-01-15 14:50:43 +0100

    voila

---
+++ Joueur.py
@@ -0,0 +1,23 @@
+from main import clear
+
+#aae : allumettes à enlever
+def jouer_coup(joueur, aae : int) -> int:
+    """Demande au joueur de jouer son coup"""
+    while valider_coup() is False:
+        print("Entrez un nombre valide d'allumettes !")
+        aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
+    clear()
+    print(f"{"|"*(NbrAllumettes-aae)}")
+    return aae
+    
+
+
+def valider_coup(aae: int) -> bool:
+    """Vérifie si le coup joué est valide"""
+    return <=0 aae <=3
+    
+
+
+def tester_victoire(NbrAllumettes : int, aae: int) -> bool:
+    """Teste si le coup résulte à une victoire"""
+    return (NbrAllumettes-aae) <= 0
