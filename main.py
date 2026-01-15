1407eefebfa1496a7dc8ca1938cb5bf7f1efe7aa
Laporte-Bidet Raphaël <rlbidet@laposte.net>
2026-01-15 15:43:42 +0100

    voila

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import os
 import plateau
 import joueur
 import mp3play
@@ -12,7 +13,22 @@ def clear() -> None:
 
 clear()
 
-#while not joueur.tester_victoire(NbrAllumettes, aae):
+NbrAllumettes = plateau.generer_partie()
+plateau.afficher_plateau(NbrAllumettes , 0)
+aae = joueur.jouer_coup
+if joueur.tester_victoire(aae) == True:
+    NbrAllumettes = NbrAllumettes - aae
+else:
+    print("joueur ", str(not joueur), " a gagné")
+    réponse = ""
+    while réponse != "oui":
+        réponse = input("Rejouer? ")
+
+
+#plateau.generer_partie()
+#joueur1 = True
+
+#while not joueur.tester_victoire():
 #    aae : int = int(input("Combien d'allumettes voulez-vous enlever (entre 1 et 3) ? : "))
 #    joueur.jouer_coup(joueur, aae)
-#    joueur1 : bool = not joueur1
+#    joueur1 = not joueur1
