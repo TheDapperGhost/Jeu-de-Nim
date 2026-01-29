0fb8462952fb29f4a02032422de853d4f697f905
TheDapperGhost <lemercierben09@gmail.com>
2026-01-29 14:22:32 +0000

    started adding asserts

---
+++ asserts.py
@@ -0,0 +1,21 @@
+import builtins
+import joueur
+
+
+def test_valider_coup():
+    assert joueur.valider_coup(1) is True
+    assert joueur.valider_coup(3) is True
+    assert joueur.valider_coup(4) is False
+    assert joueur.valider_coup(-1) is False
+
+
+def test_tester_victoire():
+    assert joueur.tester_victoire(3, 3) is True
+    assert joueur.tester_victoire(2, 1) is False
+
+
+
+
+test_valider_coup()
+test_tester_victoire()
+print("YAYYYY TOUT MARCHE")
