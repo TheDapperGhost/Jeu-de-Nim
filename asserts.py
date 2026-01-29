1d88be4eec1b6f644a80053510b36dc03b8ead36
Laporte-Bidet Raphaël <rlbidet@laposte.net>
2026-01-29 15:24:39 +0100

    voila

--- asserts.py
+++ asserts.py
@@ -1,6 +1,4 @@
-import builtins
 import joueur
-
 
 def test_valider_coup():
     assert joueur.valider_coup(1) is True
@@ -8,13 +6,9 @@ def test_valider_coup():
     assert joueur.valider_coup(4) is False
     assert joueur.valider_coup(-1) is False
 
-
 def test_tester_victoire():
     assert joueur.tester_victoire(3, 3) is True
     assert joueur.tester_victoire(2, 1) is False
-
-
-
 
 test_valider_coup()
 test_tester_victoire()
